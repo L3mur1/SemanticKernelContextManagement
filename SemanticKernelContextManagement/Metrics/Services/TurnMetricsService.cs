efaf5530e5a21903b353495a4e8d4efe8d5257ca
Adam Lenarcik <alenarcik@gmail.com>
2026-04-03 15:00:20 +0200

    added

---
+++ SemanticKernelContextManagement/Metrics/Services/TurnMetricsService.cs
@@ -0,0 +1,14 @@
+﻿using SemanticKernelContextManagement.Metrics.Models;
+
+namespace SemanticKernelContextManagement.Metrics.Services
+{
+    public class TurnMetricsService
+    {
+        private readonly List<TurnMetrics> turnMetrics = [];
+
+        public void BeginTurn() => turnMetrics.Add(new TurnMetrics()
+        {
+            Turn = turnMetrics.Count + 1,
+        });
+    }
+}
